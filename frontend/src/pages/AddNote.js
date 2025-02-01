6bfe3a391fca5f6a17ec3e58ea93953cec4fdc6e
Sourabh verma <150373765+sourabh1211@users.noreply.github.com>
2025-02-01 15:48:21 +0530

    Update AddNote.js

--- frontend/src/pages/AddNote.js
+++ frontend/src/pages/AddNote.js
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar';
 import CheckBox from '../tools/checkBox';
 import { json, useNavigate } from 'react-router-dom';
 
-const AddNote = () => {
+const AddNotes = () => {
   const editorRef = useRef(null);
 
   const [content, setContent] = useState('');
