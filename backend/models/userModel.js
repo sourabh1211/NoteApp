8ad7fc22cab6e3d47a3a0ef38fde3f0fa8866101
Sourabh verma <150373765+sourabh1211@users.noreply.github.com>
2025-02-03 20:18:17 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/noteApp");
+mongoose.connect("mongodb+srv://sourabh:egUg5OBCk16z13cN@test.b6yopve.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Test");
 
 const userSchema = new mongoose.Schema({
   username: String,
